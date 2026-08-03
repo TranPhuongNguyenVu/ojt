6ccbde7fc0a8cceedd778e2d1b72da0b507036dc
Tran Phuong Nguyen Vu <vutpnse172225@fpt.edu.vn>
2026-08-03 15:39:08 +0700

    Delete FE/movie-theater-fe directory

--- FE/movie-theater-fe/src/services/AuthService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const AuthService = axios.create({
-  baseURL: "http://localhost:8080/api/",
-  // DÒNG NÀY LÀ LINH HỒN CỦA TÍNH NĂNG: Ép Trình duyệt kẹp Cookie gửi đi!
-  withCredentials: true,
-});
-
-export default AuthService;
