57fbf89b86c929520f5cf52cbeaeba5e119f71c7
Tran Phuong Nguyen Vu <vutpnse172225@fpt.edu.vn>
2026-08-07 08:28:25 +0700

    Add files via upload

--- src/main/java/org/example/be/config/WebSocketConfig.java
+++ src/main/java/org/example/be/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${ws.allowed-origins}")
+    @Value("${WS_ALLOWED_ORIGINS:http://localhost:*,https://ojt-git-fe-tranphuongnguyenvus-projects.vercel.app}")
     private String allowedOrigins;
 
     @Override
